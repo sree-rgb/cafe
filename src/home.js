824ae2d0a90afcee53f1a6413df2ab0e4c84a937
sree-rgb <60933947+sree-rgb@users.noreply.github.com>
2021-01-28 19:21:57 +0530

    home

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 
+import {useState,useEffect} from 'react';
 function Card(props){
 
 	return (
@@ -16,17 +17,34 @@ function OfferBanner(props){
 					<h1> FREE BEVERAGE</h1><br/>
 				<h4>For an entire month</h4>
 			</div>
+      
 		</div>
 		)
 }
+function* imageNameGenerator() {
+  
+    let coffeeimages=['coffee-1.png','coffee-2.png']
+  
+  let number_of_images=coffeeimages.length
+  let current_image_index=-1
+  while (true) {
+  current_image_index= ((current_image_index+1)==number_of_images)? 0:current_image_index+1
+  yield coffeeimages[current_image_index]
+  }
+
+}
+ let image_gen=imageNameGenerator()
 function Home() {
+ 
+  let [imageGetter,setImage]=useState('coffee-1.png')
+  useEffect(()=>{
+    let clearID=setInterval(()=>{setImage(image_gen.next().value)},5000)
+  },[])
+
   return (
   	<>
   	<div  className='home_container'>
-    	<div className='item1'>
-    		<h1>Your cup of inspiration</h1>
-    		<h4>Experience love at first sip</h4>
-    	</div>
+      <img src={imageGetter} />
     	<div className="cardbox">
     	<Card src={'coffee1.svg'} text={'Espresso'}/>
     	<Card src={'coffee2.svg'} text={'Hot or Iced'}/>
